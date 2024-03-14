c0c0b9c59a16541601cbc746e4fad901a181f718
JKrsna03 <jktipsie0315@gmail.com>
2024-03-15 06:42:15 +0800

    added new file

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-console.log("Hello World");
-
-function generateRandomNumber()
+function generateRandomNumber(max)
 {
-    //return random number 0 to 10
-    let rndmNumber = (Math.floor(Math.random() * 15) + 1);
-
+    //return random number 0 to 15
+    let rndmNumber = Math.floor(Math.random() * max) + 1;
+    
     //if .... else if .... else statement
     //if(condition) {block of code to execute}
     // ==, ! =, >, <, > =, < =, &&, ||
-
-    if(rndmNumber >= 5) 
+    
+    if(rndmNumber <= 15) 
     {
-        console.log(`Random Number is >= to 5`)
+       return console.log(`Random Number ${rndmNumber} is in B. 1-15`)
+    }
+    else if (rndmNumber >= 16 && rndmNumber <= 30)
+    {
+        return console.log(`Random number ${rndmNumber} is in I. 16-30`)
+    }
+    else if (rndmNumber >= 31  && rndmNumber <= 45) 
+    {
+        return console.log(`Random number ${rndmNumber} is in N. 31-45`)
+    }
+    else if (rndmNumber >= 46 && rndmNumber <= 60 ) 
+    {
+        return console.log(`Random number ${rndmNumber} is in G. 46-61`)
+    }
+    else if (rndmNumber >= 61 && rndmNumber <=75)
+    {
+        return console.log(`Random number ${rndmNumber} is in O. 61-75`)
     }
     else
     {
-        console.log(`Random Number is <= to 5`)
+        return console.log(`Random Number is invalid`)
     }
 
-    return console.log(rndmNumber);
 }
+
+generateRandomNumber(75);
+console.log("Hello World");
